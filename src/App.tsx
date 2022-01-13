534a7e1b0f1f78a71cc685fd34fe406650e3517b
baebumsoo <baebeomsu77@gmail.com>
2022-01-13 20:31:50 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import QuestionCard from './components/QuestionCard';
 
-function App() {
-   const app = 0;
-   app = 1;
+const startTrivia = () => {
+
+}
+
+const checkAnswer = () => {
+
+}
+
+const nextQuestion = () => {
+  
+}
+
+const App = () => {
+   const [loading, setLoading] = useState(false);
+   const [questions, setQuestions] = useState([]);
+   const [number, setNumber] = useState(0);
+   const [userAnswers, setUserAnswers] = useState([]);
+   const [score, setScore] = useState(0);
+   const [gameOver, setGameOver] = useState(true);
+   
+   return (
+   <div className='App'>
+      <h1>REACT QUIZ</h1>
+      <button className='start' onClick={startTrivia}>
+         Start
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCard questionNr={number + 1} />
+   </div>
+   )
 }
 
 export default App;
